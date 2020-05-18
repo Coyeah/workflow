92aaf3736926135035dd8f9ce69ef3c8073a044c
Coyeah <Coyeah_chen@outlook.com>
2020-05-18 17:47:43 +0800

    updated: remove image loader

--- webpack/webpack.common.config.js
+++ webpack/webpack.common.config.js
@@ -64,27 +64,6 @@ module.exports = {
                 limit: 8192,
                 name: `images/[name]-[hash:5].${website.name}.[ext]`
               }
-            }, {
-              loader: "image-webpack-loader",
-              options: {
-                mozjpeg: { // 压缩 jpeg 的配置
-                  progressive: true,
-                  quality: 65
-                },
-                optipng: { // 使用 imagemin-optipng 压缩 png，enable: false 为关闭
-                  enabled: false,
-                },
-                pngquant: { // 使用 imagemin-pngquant 压缩 png
-                  quality: '65-90',
-                  speed: 4
-                },
-                gifsicle: { // 压缩 gif 的配置
-                  interlaced: false,
-                },
-                webp: { // 开启 webp，会把 jpg 和 png 图片压缩为 webp 格式
-                  quality: 75
-                },
-              }
             }
           ]
         }, {
